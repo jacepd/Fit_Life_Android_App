a22c70a5021744467cb081387dc529a84c471c93
JohnMismash <u1179865@utah.edu>
2022-01-27 20:07:41 -0700

    New Project.

---
+++ app/src/main/java/com/example/cs4350lectureexamples/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cs4350lectureexamples;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
