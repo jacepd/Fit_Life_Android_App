dfcda0057043a3afc4e09e8e541b2c6cea5758bb
bvanhoose14 <54387980+bvanhoose14@users.noreply.github.com>
2022-01-30 14:43:56 -0700

    first activity

--- Project1/app/src/main/java/com/example/basicinfoname/MainActivity.java
+++ Project1/app/src/main/java/com/example/basicinfoname/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.basicinfoname;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener{
+
+    private Button mButtonSubmit;
+    private EditText mFirst_input;
+    private EditText mLast_input;
+    private String mFirstName;
+    private String mLastName;
+    private String mDisplayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get the button
+        mButtonSubmit = (Button) findViewById(R.id.button_submit);
+        mButtonSubmit.setOnClickListener(this);
+
+        //Get the EditText
+        mFirst_input = (EditText) findViewById(R.id.firstName_input);
+        mLast_input = (EditText) findViewById(R.id.lastName_input);
+
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.button_submit:
+                mDisplayText = mFirst_input.getText().toString();
+        }
     }
 }
