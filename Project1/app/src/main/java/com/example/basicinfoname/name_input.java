fd91764a26b3c5da4864bd7d4eb0c8a2fe13897d
bvanhoose14 <54387980+bvanhoose14@users.noreply.github.com>
2022-01-31 10:01:27 -0700

    got app loading and first activity displaying submitted names

--- Project1/app/src/main/java/com/example/basicinfoname/name_input.java
+++ Project1/app/src/main/java/com/example/basicinfoname/name_input.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class name_input extends AppCompatActivity
         implements View.OnClickListener{
@@ -29,7 +30,6 @@ public class name_input extends AppCompatActivity
         //Get the EditText
         mFirst_input = (EditText) findViewById(R.id.firstName_input);
         mLast_input = (EditText) findViewById(R.id.lastName_input);
-
     }
 
     @Override
@@ -37,7 +37,15 @@ public class name_input extends AppCompatActivity
     {
         switch (view.getId()) {
             case R.id.button_submit:
-                mDisplayText = mFirst_input.getText().toString();
+
+                //Error checking:
+                // make sure its a valid name and it's a string
+                // account for weird last names with spaces and hyphens
+
+                String first = mFirst_input.getText().toString();
+                String last = mLast_input.getText().toString();
+                TextView tv = (TextView)findViewById(R.id.display_text);
+                tv.setText(first + " " + last);
         }
     }
 }
