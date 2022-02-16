1235230b825aba920c083785a17e71961223eaa1
JohnMismash <u1179865@utah.edu>
2022-02-16 12:57:56 -0700

    Added weather page

---
+++ Project1/app/src/main/java/com/example/Fit_Life/weather_output.java
@@ -0,0 +1,16 @@
+package com.example.Fit_Life;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.basicinfoname.R;
+
+public class weather_output extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_output);
+    }
+}
