52afdf713d21cc1b9c62f58397eba8c3ad1eb593
JohnMismash <u1179865@utah.edu>
2022-02-23 22:51:12 -0700

    Update JSONWeatherUtils.java

--- Project1/app/src/main/java/com/example/Fit_Life/JSONWeatherUtils.java
+++ Project1/app/src/main/java/com/example/Fit_Life/JSONWeatherUtils.java
@@ -8,9 +8,25 @@ import org.json.JSONObject;
 public class JSONWeatherUtils {
     public static WeatherData getWeatherData(String data) throws JSONException{
         WeatherData weatherData = new WeatherData();
+        LocationData locationData = new LocationData();
 
         //Start parsing JSON data
         JSONObject jsonObject = new JSONObject(data); //Must throw JSONException
+
+        // Get location data
+        JSONObject jsonCoords = jsonObject.getJSONObject("coord");
+        locationData.setLatitude(jsonCoords.getDouble("lat"));
+        locationData.setLongitude(jsonCoords.getDouble("long"));
+
+        JSONObject jsonSys = jsonObject.getJSONObject("sys");
+        locationData.setCountry(jsonSys.getString("country"));
+        locationData.setCity(jsonObject.getString("name"));
+
+        weatherData.setLocationData(locationData);
+
+
+
+
 
         WeatherData.CurrentCondition currentCondition = weatherData.getCurrentCondition();
         JSONObject jsonMain = jsonObject.getJSONObject("main");
