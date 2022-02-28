651c65785a51cadbd7c94ba35e76bfbe586bff02
JohnMismash <u1179865@utah.edu>
2022-02-27 19:53:38 -0700

    Fixed building the URL with a city with spaces.

--- Project1/app/src/main/java/com/example/Fit_Life/weather_output.java
+++ Project1/app/src/main/java/com/example/Fit_Life/weather_output.java
@@ -2,27 +2,19 @@ package com.example.Fit_Life;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.os.HandlerCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.basicinfoname.R;
-
 import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.net.URL;
+import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -66,21 +58,49 @@ public class weather_output extends AppCompatActivity implements View.OnClickLis
         mButtonUpdate.setOnClickListener(this);
 
         mTvTemp = (TextView) findViewById(R.id.tv_temp_val);
-        mTvMaxTemp = (TextView) findViewById(R.id.tv_temp_val);
-        mTvMinTemp = (TextView) findViewById(R.id.tv_temp_val);
+        // mTvMaxTemp = (TextView) findViewById(R.id.tv_temp_val);
+        // mTvMinTemp = (TextView) findViewById(R.id.tv_temp_val);
         mTvRainAmount = (TextView) findViewById(R.id.tv_amount_of_rain);
 
         mEtLocation = (EditText) findViewById(R.id.et_location);
 
         allDataStr = helperMethods.readData(this);
-        String[] data = allDataStr.split(",");
+
+        String[] split_data = allDataStr.split(",");
+
+        city = split_data[7];
+        state = split_data[8];
+
+        String[] fullCity = null;
+        StringBuilder location = new StringBuilder();
+
+        if (city.contains(" ")) {
+            fullCity = city.split(" ");
+
+            for(int i = 0; i < fullCity.length; i++){
+
+                if (i > 0){
+                    location.append("&").append(fullCity[i]);
+                }
+
+                else {
+                    location.append(fullCity[i]);
+                }
+            }
+        }
+
+        else{ location = new StringBuilder(city); }
+
+        location.append(",us");
 
         // Begin thread here
-        loadWeatherData(allDataStr);
+        loadWeatherData(location.toString());
+
+        int i = 0;
     }
 
-    private void loadWeatherData(String allDataStr){
-        new FetchWeatherTask().execute(allDataStr);
+    private void loadWeatherData(String location){
+        new FetchWeatherTask().execute(location);
     }
 
     @Override
@@ -94,39 +114,30 @@ public class weather_output extends AppCompatActivity implements View.OnClickLis
             case R.id.button_submit:
                 String inputFromEt = mEtLocation.getText().toString().replace(
                         ' ', '&');
+
                 loadWeatherData(inputFromEt);
         }
     }
 
+    /*
+    *
+    *
+    *
+    *
+    */
     private class FetchWeatherTask {
 
         public ExecutorService executorService = Executors.newSingleThreadExecutor();
         public Handler mainThreadHandler = HandlerCompat.createAsync(Looper.getMainLooper());
 
-        public void execute(String... data) {
+        public void execute(String... location) {
 
             executorService.execute(() -> {
 
-                city = data[7];
-                state = data[8];
-
-                String[] fullCity = null;
-                String location = null;
-
-                if (city.contains(" ")) {
-                    fullCity = city.split(" ");
-
-                    for(int i = 0; i < fullCity.length; i++){
-                        location = "" + fullCity[i];
-                    }
-                }
-
-                else{ location = city; }
-
-                location = location + "," + state;
+                // Toast.makeText(weather_output.this, "" + location, Toast.LENGTH_SHORT).show();
 
                 // location = "Salt&Lake&City,us";
-                URL weatherDataURL = NetworkUtils.buildURLFromString(location);
+                URL weatherDataURL = NetworkUtils.buildURLFromString("" + location);
 
                 String jsonWeatherData = null;
 
