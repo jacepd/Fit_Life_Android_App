7c69261234191376c55efcf56b0c96c79a774d5a
JohnMismash <u1179865@utah.edu>
2022-02-27 18:01:02 -0700

    AsyncTask is deprecated, so had to re-work the multi-threading to handle retrieval of the weather data.

--- Project1/app/src/main/java/com/example/Fit_Life/weather_output.java
+++ Project1/app/src/main/java/com/example/Fit_Life/weather_output.java
@@ -1,12 +1,15 @@
 package com.example.Fit_Life;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.os.HandlerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -19,26 +22,21 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class weather_output extends AppCompatActivity implements View.OnClickListener{
 
     private Button mButtonReturn;
     private WeatherData mWeatherData;
-    private WeatherData.Temperature mTemp;
+    private TextView mTvMaxTemp = (TextView) findViewById(R.id.tv_temp_val);
+    private TextView mTvMinTemp = (TextView) findViewById(R.id.tv_temp_val);
+    private double mTemp;
     private double mMaxTemp;
-    private WeatherData.CurrentCondition mCurrentCondition;
+    private double mMinTemp;
     private String allDataStr;
-    private String firstName;
-    private String lastName;
-    private int age;
-    private int weight;
-    private int heightFeet;
-    private int heightInches;
-    private String sex;
     private String city;
     private String state;
-    private String goal;
-    private String activityLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,19 +56,18 @@ public class weather_output extends AppCompatActivity implements View.OnClickLis
 
         TextView mLoc_val = (TextView) findViewById(R.id.tv_loc_val);
 
-        TextView mMaxTemp_val = (TextView) findViewById(R.id.tv_temp_val);
-        // TextView mMinTemp_val = (TextView) findViewById(R.id.tv_temp_val);
 
         TextView mRainAmount_val = (TextView) findViewById(R.id.tv_amount_of_rain);
 
         allDataStr = helperMethods.readData(this);
         String[] data = allDataStr.split(",");
 
-        mTemp = mWeatherData.getTemperature();
-        mMaxTemp = mTemp.getMaxTemp();
+        // Begin thread here
+        loadWeatherData(allDataStr);
+    }
 
-        mMaxTemp_val.setText((int) mMaxTemp);
-
+    private void loadWeatherData(String allDataStr){
+        new FetchWeatherTask().execute(allDataStr);
     }
 
     @Override
@@ -84,40 +81,66 @@ public class weather_output extends AppCompatActivity implements View.OnClickLis
         }
     }
 
-    private class FetchWeatherTask extends AsyncTask<String, Void, String> {
+    private class FetchWeatherTask {
 
-        @Override
-        protected String doInBackground(String... data) {
+        public ExecutorService executorService = Executors.newSingleThreadExecutor();
+        public Handler mainThreadHandler = HandlerCompat.createAsync(Looper.getMainLooper());
 
-            // Get City/State from user file.
-            city = data[11];
-            state = data[12];
+        public void execute(String... data) {
 
-            String location = "Salt&Lake&City,us";
-            URL weatherDataURL = NetworkUtils.buildURLFromString(location);
+            executorService.execute(() -> {
 
-            String jsonWeatherData = null;
-            try {
-                jsonWeatherData = NetworkUtils.getDataFromURL(weatherDataURL);
-                return jsonWeatherData;
+                city = data[7];
+                state = data[8];
 
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
+                String[] fullCity = null;
+                String location = null;
+
+                if (city.contains(" ")) {
+                    fullCity = city.split(" ");
+
+                    for(int i = 0; i < fullCity.length; i++){
+                        location = "" + fullCity[i];
+                    }
+                }
+
+                else{ location = city; }
+
+                location = location + "," + state;
+
+                // location = "Salt&Lake&City,us";
+                URL weatherDataURL = NetworkUtils.buildURLFromString(location);
+
+                String jsonWeatherData = null;
+
+                try {
+                    jsonWeatherData = NetworkUtils.getDataFromURL(weatherDataURL);
+                    postToMainThread(jsonWeatherData);
+                }
+                catch (Exception e) { e.printStackTrace(); }
+
+
+            });
         }
 
-        @Override
-        protected void onPostExecute(String jsonWeatherData) {
-            if (jsonWeatherData != null){
-                try {
-                    mWeatherData = JSONWeatherUtils.getWeatherData(jsonWeatherData);
+        public void postToMainThread(String jsonWeatherData) {
+
+            mainThreadHandler.post(() -> {
+                
+                if (jsonWeatherData != null) {
+
+                    try { mWeatherData = JSONWeatherUtils.getWeatherData(jsonWeatherData); }
+                    catch (JSONException e) { e.printStackTrace(); }
+
+                    if (mWeatherData != null) {
+                        mTemp = mWeatherData.getTemperature().getTemp();
+                        mMaxTemp = mWeatherData.getTemperature().getMaxTemp();
+                        mMinTemp = mWeatherData.getTemperature().getMinTemp();
+
+                        mTvMaxTemp.setText((int) mMaxTemp);
+                    }
                 }
-                catch (JSONException e) {
-                    e.printStackTrace();
-                    return;
-                }
-            }
+            });
         }
     }
 }
