7dcf639ac13cac3f42ff52ae125a8b1cf161895b
Tony <totorobinson@gmail.com>
2022-03-21 16:53:21 -0600

    added user class

---
+++ Project1/app/src/main/java/com/example/Fit_Life/User.java
@@ -0,0 +1,105 @@
+package com.example.Fit_Life;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int weight;
+    private int heightFeet;
+    private int heightInches;
+    private String sex;
+    private String city;
+    private String state;
+    private String goal;
+    private String activityLevel;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getHeightFeet() {
+        return heightFeet;
+    }
+
+    public int getHeightInches() {
+        return heightInches;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public String getActivityLevel() {
+        return activityLevel;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setHeightFeet(int heightFeet) {
+        this.heightFeet = heightFeet;
+    }
+
+    public void setHeightInches(int heightInches) {
+        this.heightInches = heightInches;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+
+    public void setActivityLevel(String activityLevel) {
+        this.activityLevel = activityLevel;
+    }
+
+}
